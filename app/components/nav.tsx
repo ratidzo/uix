1757b5b28822c6a575918eb12da8389ea50550b9
ratidzo <ratygore@gmail.com>
2023-12-29 17:30:45 +0200

    dev: project setup - themes and icons

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,8 +1,16 @@
 
 export default function Nav() {
     return (
-        <nav>
-            Nav Section
+        <nav className="flex justify-between px-16 py-4 bg-accent-purple-extralight">
+            <div>
+                Logo
+            </div>
+            <div>
+                Links
+            </div>
+            <div>
+                Button
+            </div>
         </nav>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,38 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        text: {
+          100: '#FAFAFA',
+          200: '#CBD4E6',
+          300: '#A1B0CC',
+          400: '#7C8DB0',
+          500: '#657491',
+          600: '#6E7491',
+          700: '#52556F',
+          800: '#36374A',
+          900: '#27273F'
+        }, 
+
+        primary: {
+          'purple-blue': '#605DEC',
+          'blue': '#407AEA',
+          'purple-dark': '#1513A0'
+        }, 
+
+        accent: {
+          'purple-light': '#BCBAF7',
+          'purple-extralight': '#E9E8FC',
+          'purple-white': '#F6F6FE',
+          'pink': 'EB568C'
+        },
+
+        status: {
+          'green': '#00BC9A',
+          'dark-green': '#00B666',
+          'danger': 'EB5757'
+        }
+      }
     },
   },
   plugins: [],
