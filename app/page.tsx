e16bec98293a57807103e5c4ee107fa72156dfea
ratidzo <ratygore@gmail.com>
2023-12-29 19:58:00 +0200

    config: add localhost as localhost ip to remotePatterns

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+  const destinations = await getDestinations()
   return (
-    <>
-    </>
+    <div>
+      <Image alt='city' width={64} height={64} src={'http://127.0.0.1:8000/media/destination_images/Sydney_wFs4nre.png'}/>
+    </div>
   )
+}
+
+
+async function getDestinations() {
+  const data = await fetch('http://127.0.0.1:8000/destinations/',
+    {
+      method: 'GET',
+      
+      next: {
+        revalidate: 5
+      }
+    },
+   
+  );
+  return data.json()
 }
