e7e2b59faec06da6d476ce5d78c1554bd1d7ee55
ratidzo <ratygore@gmail.com>
2023-12-29 17:48:03 +0200

    dev: setup brand - logo and icon

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,9 +1,27 @@
+import { Nunito_Sans } from "next/font/google"
+import Image from "next/image"
+
+const nunito_sans_bold = Nunito_Sans({
+    subsets: ['latin'],
+    weight: '700'
+})
+
+
+const nunito_sans = Nunito_Sans({
+    subsets: ['latin'],
+    weight: '400'
+})
 
 export default function Nav() {
     return (
-        <nav className="flex justify-between px-16 py-4 bg-accent-purple-extralight">
-            <div>
-                Logo
+        <nav className="flex items-center justify-between px-16 py-4 bg-accent-purple-extralight">
+            <div className={`flex items-center gap-1 ${nunito_sans_bold.className} text-primary-purple-blue
+            text-4xl`}>
+                <h1>
+                    Fleit
+                </h1>
+                <Image alt="Brand Icon" src={'plane.svg'}
+                width={32} height={32}/>
             </div>
             <div>
                 Links
